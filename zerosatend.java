12d558b773380cd8403b13337842165b3875ee25
KeerthiKola19 <keerthikola119@gmail.com>
2026-01-20 10:16:39 +0530

    Add files via upload

---
+++ zerosatend.java
@@ -0,0 +1,18 @@
+
+
+//Java Solution in O(N) time complexity.
+
+
+// User function Template for Java
+
+class zerosatend {
+    // Just print the space seperated array elements
+    void printArray(int arr[]) {
+        
+        for(int i = 0; i < arr.length; i++){
+            
+            System.out.print(arr[i] + " ");
+        }
+        
+    }
+}
